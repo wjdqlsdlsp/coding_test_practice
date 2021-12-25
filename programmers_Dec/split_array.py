69c359fc8d9c0937cee68f3e5cc8a8de6f07c9fc
Jeongbin Park <wjdqlsdlsp@naver.com>
2021-12-25 19:44:45 +0900

    211225

---
+++ programmers_Dec/split_array.py
@@ -0,0 +1,15 @@
+def solution(n, left, right):
+
+    answer = [max(i//n, i%n)+1 for i in range(left, right+1)]
+    return answer
+
+# 시간초과 코드
+# def solution(n, left, right):
+#     arr = [[max((j+1),(i+1)) for j in range(n)] for i in range(n)]
+#     t = []
+#     for i in arr:
+#         t+=i
+#     return t[left:right+1]
+
+
+print(solution(3,2,5))
