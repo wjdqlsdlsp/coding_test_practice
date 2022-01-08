57195249dafa6480442a2db9d5cf1107c1252d98
Jeongbin Park <wjdqlsdlsp@naver.com>
2022-01-08 15:34:10 +0900

    210108

---
+++ BOJ/1038.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+# a = int(input())
+a = 18
+
+result = []
+for i in range(1,11):
+    for tmp in combinations(range(0,10), i):
+        tmp = list(tmp)
+        tmp.sort(reverse=True)
+        result.append(int("".join(map(str, tmp))))
+
+result.sort()
+
+
+if len(result) < a:
+    print(-1)
+else:
+    print(result[a])
