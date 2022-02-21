15de6c98a9ff84dc95334e51c54920b350761eca
Jeongbin Park <wjdqlsdlsp@naver.com>
2022-02-21 15:13:29 +0900

    220221

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import time
-import math
-start = time.time()
-math.perm(1000)
-print("perm: (100)" ,time.time() - start)
-
-start = time.time()
-math.factorial(10000)
-print("factorial: (100)" ,time.time() - start)
+print(-1% 10)
